6b61f1dd1c3e4af020ae45bfcd2cb9c5ac448ed9
Ivan Krechetov <ivan.krechetov@gmail.com>
2012-09-07 08:30:10 +0200

    Another example

--- 08.js
+++ 08.js
@@ -1,4 +1,13 @@
 
 // :(
 
+this.foo = 42;
+this.bar = seeminglyComplexCalculation(this.foo);
+
 // :)
+
+this.foo = 42;
+
+this.seeminglyComplexCalculation = function () {
+    return compute(... this.foo ...);
+};
